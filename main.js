3ea576dd40a9f77be52d046424d23185c99f0edc
lolpoperman20122 <135189555+lolpoperman20122@users.noreply.github.com>
2023-12-16 20:13:14 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,4 +5,8 @@ $(document).ready(function() {
         $('#proxyResult').show();
         $('#proxyFrame').attr('src', 'https://cors-anywhere.herokuapp.com/' + siteUrl);
     });
-});
+});var x = new XMLHttpRequest();
+x.open('GET', 'https://cors-anywhere.herokuapp.com/http://www.thecapitoltheatre.com/files/2016/01/squirrel1.jpg');
+//x.setRequestHeader('x-requested-with', 'XMLHTTPREQUEST');x.responseType = 'blob';
+x.onload = function () {..........};
+x.onerror = function () {........}
