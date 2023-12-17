9b90af01a893b27d154c4240fc0a91f86cd94d70
lolpoperman20122 <135189555+lolpoperman20122@users.noreply.github.com>
2023-12-16 20:21:31 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
-$(document).ready(function() {
-    $('#proxySiteForm').submit(function(e) {
-        e.preventDefault();
-        var siteUrl = $('#siteUrl').val();
-        $('#proxyResult').show();
-        $('#proxyFrame').attr('src', 'https://cors-anywhere.herokuapp.com/' + siteUrl);
-    });
-});var x = new XMLHttpRequest();
-x.open('GET', 'https://cors-anywhere.herokuapp.com/http://www.thecapitoltheatre.com/files/2016/01/squirrel1.jpg');
-//x.setRequestHeader('x-requested-with', 'XMLHTTPREQUEST');x.responseType = 'blob';
-x.onload = function () {..........};
-x.onerror = function () {........}
+$(function() {
+  $.ajax({
+    url: "https://cors-anywhere.herokuapp.com/$(function() {
+  $.ajax({
+    url: "https://cors-anywhere.herokuapp.com/",
+    type: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    // set the request header authorization to the bearer token that is generated
+    headers: {
+      "X-Requested-With": "XMLHttpRequest"
+    },
+    success: function(result) {
+      console.log(result);
+
+    },
+    error: function(error) {
+      console.log(`Error ${error}`)
+    },
+  });
+});",
+    type: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    // set the request header authorization to the bearer token that is generated
+    headers: {
+      "X-Requested-With": "XMLHttpRequest"
+    },
+    success: function(result) {
+      console.log(result);
+
+    },
+    error: function(error) {
+      console.log(`Error ${error}`)
+    },
+  });
+});
